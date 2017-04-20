ebb8be6b647afa549701220d7e225315b8bc858a
cjoonh <cjoonh@gmail.com>
2017-04-20 17:26:37 +0900

    Add files via upload

--- naver_top.R
+++ naver_top.R
@@ -1,10 +1,15 @@
+#네이버 실시간 상위 검색어 출력 함수입니다
+#L=필요한 순위 변수 입력(1~20)
+
+navertop <- function(L) {
 text=readLines("http://www.naver.com/")
 Encoding(text)="UTF-8"
 text_sub=text[434:553]
-L=10 # 필요한 순위 변수 설정(1~20)
-text_sub2<-rep(0,L)
+
+navertop_text<-rep(0,L)
 for(n in 1:L){
   start_p=gregexpr(text_sub[(n*6)], pattern=">")[[1]][1]+1
   end_p=gregexpr(text_sub[(n*6)], pattern="<")[[1]][2]-1
-  text_sub2[n]=substr(text_sub[(n*6)], start_p, end_p)}
-text_sub2
+  navertop_text[n]=substr(text_sub[(n*6)], start_p, end_p)}
+return(navertop_text)
+}
